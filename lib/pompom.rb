716a38bd1f213350047ea7d0a8f47a669db0490d
Roland Swingler <roland.swingler@gmail.com>
2012-05-09 11:02:17 +0100

    Adding an NCurses screen.

--- lib/pompom.rb
+++ lib/pompom.rb
@@ -6,3 +6,21 @@ def format_time(number)
   seconds = number % 60
   sprintf("%02d:%02d", minutes, seconds)
 end
+
+module Pompom
+  class NCursesScreen
+    include FFI::NCurses
+
+    def run
+      begin
+        initscr
+        cbreak
+        noecho
+        curs_set 0
+        yield
+      ensure
+        endwin
+      end
+    end
+  end
+end
