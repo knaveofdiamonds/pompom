d7fd605fcbbca0d15bcedb1c38b2a38a59399391
Roland Swingler <roland.swingler@gmail.com>
2012-05-09 11:05:03 +0100

    namespacing format_time method.

--- lib/pompom.rb
+++ lib/pompom.rb
@@ -1,13 +1,13 @@
 require 'ffi-ncurses'
 require 'artii'
 
-def format_time(number)
-  minutes = number / 60
-  seconds = number % 60
-  sprintf("%02d:%02d", minutes, seconds)
-end
-
 module Pompom
+  def self.format_time(number)
+    minutes = number / 60
+    seconds = number % 60
+    sprintf("%02d:%02d", minutes, seconds)
+  end
+  
   class NCursesScreen
     include FFI::NCurses
 
--- test/test_pompom.rb
+++ test/test_pompom.rb
@@ -1,19 +1,19 @@
 require 'helper'
   
-describe "Pompom" do
+describe "Pompom helper methods" do
   it "should format 0 as 00:00" do
-    format_time(0).must_equal "00:00"
+    Pompom.format_time(0).must_equal "00:00"
   end
 
   it "should format 1 second as 00:01" do
-    format_time(1).must_equal "00:01"
+    Pompom.format_time(1).must_equal "00:01"
   end
 
   it "formats 60 seconds as 01:00" do
-    format_time(60).must_equal "01:00"
+    Pompom.format_time(60).must_equal "01:00"
   end
 
   it "formats 61 seconds as 01:01" do
-    format_time(61).must_equal "01:01"
+    Pompom.format_time(61).must_equal "01:01"
   end
 end
